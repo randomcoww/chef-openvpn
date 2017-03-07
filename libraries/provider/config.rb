f1e05ca81ec9b88eba61c83ab353a9592df14ead
randomcoww <etherblimp@gmail.com>
2017-03-06 22:51:32 -0800

    fix

--- libraries/provider/config.rb
+++ libraries/provider/config.rb
@@ -1,40 +1,41 @@
 class ChefOpenvpn
   class Provider
     class Config < Chef::Provider
-    provides :openvpn_config, os: "linux"
+      provides :openvpn_config, os: "linux"
 
-    def load_current_resource
-      @current_resource = ChefNsd::Resource::Config.new(new_resource.name)
+      def load_current_resource
+        @current_resource = ChefNsd::Resource::Config.new(new_resource.name)
 
-      current_resource.exists(::File.exist?(new_resource.path))
+        current_resource.exists(::File.exist?(new_resource.path))
 
-      if current_resource.exists
-        current_resource.content(::File.read(new_resource.path).chomp)
-      else
-        current_resource.content('')
+        if current_resource.exists
+          current_resource.content(::File.read(new_resource.path).chomp)
+        else
+          current_resource.content('')
+        end
+
+        current_resource
       end
 
-      current_resource
-    end
+      def action_create
+        converge_by("Create OpenVPN config: #{new_resource}") do
+          nsd_config.run_action(:create)
+        end if !current_resource.exists || current_resource.content != new_resource.content
+      end
 
-    def action_create
-      converge_by("Create OpenVPN config: #{new_resource}") do
-        nsd_config.run_action(:create)
-      end if !current_resource.exists || current_resource.content != new_resource.content
-    end
+      def action_delete
+        converge_by("Delete OpenVPN config: #{new_resource}") do
+          nsd_config.run_action(:delete)
+        end if current_resource.exists
+      end
 
-    def action_delete
-      converge_by("Delete OpenVPN config: #{new_resource}") do
-        nsd_config.run_action(:delete)
-      end if current_resource.exists
-    end
+      private
 
-    private
-
-    def openvpn_config
-      @openvpn_config ||= Chef::Resource::File.new(new_resource.path, run_context).tap do |r|
-        r.path new_resource.path
-        r.content new_resource.content
+      def openvpn_config
+        @openvpn_config ||= Chef::Resource::File.new(new_resource.path, run_context).tap do |r|
+          r.path new_resource.path
+          r.content new_resource.content
+        end
       end
     end
   end
